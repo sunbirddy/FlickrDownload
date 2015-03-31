8dd34bc4c47bf437758dd91d8f322b32314033a8
yzloveyz <yzloveyz@outlook.com>
2015-03-30 17:47:54 -0700

    Add comments and change API keys to XXX

--- flickrDownload.py
+++ flickrDownload.py
@@ -1,32 +1,41 @@
-import flickrapi
+import flickrapi  # Beej's Python Flickr API from http://stuvel.eu/flickrapi
 import urllib, urlparse
 import os
 # import socket
 
 if __name__ == '__main__':
 
-    api_key = 'd80e22556ee7270de4648a8d74e902c3'
-    api_secret = 'b46e39656fee2b2e'
+    api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXX'  # Obtained from https://www.flickr.com/services/api/
+    api_secret = 'XXXXXXXXXXXXXXXXX'
     flickr = flickrapi.FlickrAPI(api_key, api_secret, cache=True)
 
     try:
-        photos = flickr.walk(# text = "stanford",
+        photos = flickr.walk(# Name
+                             # text = "stanford",
+                             # geo-location by locating bounding box
                              # bbox = {-122.181885, 37.422008, -122.160299, 37.436765},
-                             # bbox = {-180.0,-90.0,180.0,90.0},
+                             # geo-location by specifying lat/lon and radius
                              lat = '37.428620',
                              lon = '-122.170723',
                              radius = '2',
+                             # Taken time
                              min_taken_date = '2014-12-23',
                              max_taken_date = '2014-12-30',
+                             # Accuracy level
                              accuracy = '16',
+                             # Content type
                              content_type = '1',
+                             # Has geotag or not?
                              has_geo = '1',
+                             # Media type: photo
                              media = 'photos',
+                             # Extra tag: url, geo coordinates
                              extras = 'url_z,geo'
                              )
     except Exception:
         print 'No photos found!'
 
+    # Open a new txt file to store meta information associated with each photo
     f = open('/home/yzhu/Downloads/imageInfo_2.txt','a+')
     exceptionNum = 0
     try:
